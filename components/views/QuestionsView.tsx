5a3afffe7968a6c83a4eb5d67a46e70a520447ef
Ricardo-Filipini <98062675+Ricardo-Filipini@users.noreply.github.com>
2025-11-05 17:43:45 -0300

    refactor: Change default sort option in QuestionsView
    The default sorting option for questions has been changed from 'time' to 'temp'. This is a temporary change to facilitate further development and testing of the question sorting functionality.

--- components/views/QuestionsView.tsx
+++ components/views/QuestionsView.tsx
@@ -1,4 +1,5 @@
 
+
 import React, { useState, useEffect, useMemo, useRef } from 'react';
 import { MainContentProps } from '../../types';
 import { Question, Comment, QuestionNotebook, UserNotebookInteraction, UserQuestionAnswer } from '../../types';
@@ -21,7 +22,7 @@ interface QuestionsViewProps extends MainContentProps {
 export const QuestionsView: React.FC<QuestionsViewProps> = ({ allItems, appData, setAppData, currentUser, updateUser, navTarget, clearNavTarget, setScreenContext }) => {
     const [selectedNotebook, setSelectedNotebook] = useState<QuestionNotebook | 'all' | null>(null);
     const [commentingOnNotebook, setCommentingOnNotebook] = useState<QuestionNotebook | null>(null);
-    const [sort, setSort] = useState<SortOption>('time');
+    const [sort, setSort] = useState<SortOption>('temp');
     const [questionIdToFocus, setQuestionIdToFocus] = useState<string | null>(null);
     
     useEffect(() => {
